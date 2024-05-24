09e9a0cbae91a876f85eded8708ef8642babc53d
Agayeva Aynur <aghayeva552@gmail.com>
2024-05-24 18:14:07 +0400

    database updated

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWcZvRY0OomWkDFjIaIvhhtQ9UaJbC04Q",
-  authDomain: "update-project-d8e66.firebaseapp.com",
-  databaseURL: "https://update-project-d8e66-default-rtdb.firebaseio.com",
-  projectId: "update-project-d8e66",
-  storageBucket: "update-project-d8e66.appspot.com",
-  messagingSenderId: "359877126507",
-  appId: "1:359877126507:web:e6aa296f5d60692483842e"
+  apiKey: "AIzaSyCv0R4IjeF9g89R0oRsvLMS5ricnty3Jl8",
+  authDomain: "ollis-project.firebaseapp.com",
+  databaseURL: "https://ollis-project-default-rtdb.firebaseio.com",
+  projectId: "ollis-project",
+  storageBucket: "ollis-project.appspot.com",
+  messagingSenderId: "225429164453",
+  appId: "1:225429164453:web:e87a09081217d24fd3d84c"
 };
 
 // Initialize Firebase
